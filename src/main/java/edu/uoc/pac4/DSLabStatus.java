0edc52727d24d70a8ead366f0ccd87a93ed750ec
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 09:37:49 +0100

    Update DSLabStatus.java
    05

--- src/main/java/edu/uoc/pac4/DSLabStatus.java
+++ src/main/java/edu/uoc/pac4/DSLabStatus.java
@@ -1,5 +1,8 @@
 package edu.uoc.pac4;
 
+/**
+ * Enumeration representing the status of a DSLab.
+ */
 public enum DSLabStatus {
     ACTIVE("Active"),
     INACTIVE("Inactive"),
@@ -7,14 +10,29 @@ public enum DSLabStatus {
 
     private final String status;
 
+    /**
+     * Constructor for DSLabStatus.
+     *
+     * @param status the status as a String
+     */
     DSLabStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Gets the status as a String.
+     *
+     * @return the status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Returns a string representation of the status in uppercase.
+     *
+     * @return the status in uppercase enclosed in square brackets
+     */
     @Override
     public String toString() {
         return "[" + status.toUpperCase() + "]";
